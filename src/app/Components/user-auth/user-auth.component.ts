d06bd83670db0823cae5705bed2c9fe1317328a5
FIRST_NAME LAST_NAME <yaman0@protonmail.com>
2019-06-15 11:16:34 +0200

    feat(auth): change mode login/register

--- src/app/Components/user-auth/user-auth.component.ts
+++ src/app/Components/user-auth/user-auth.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+
+enum Focus {
+  REGISTER,
+  LOGIN
+}
 
 @Component({
   selector: 'app-user-auth',
@@ -6,10 +12,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-auth.component.css']
 })
 export class UserAuthComponent implements OnInit {
+  focus: Focus;
+  step: string;
+  Focus = Focus;
 
-  constructor() { }
+  constructor() {
+    this.focus = Focus.LOGIN;
+    this.step = '35%';
+  }
 
   ngOnInit() {
   }
 
+  moveFocus() {
+    switch (this.focus) {
+      case Focus.LOGIN:
+        this.focus = Focus.REGISTER;
+        this.step = '-15%';
+
+        break;
+      case Focus.REGISTER:
+        this.focus = Focus.LOGIN;
+        this.step = '35%';
+        break;
+    }
+
+  }
 }
