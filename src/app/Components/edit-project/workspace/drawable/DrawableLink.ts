0b2b3b3bfcec567fd5a2b7a867df526cf1077dff
FIRST_NAME LAST_NAME <yaman0@protonmail.com>
2019-07-03 11:35:03 +0200

    feat(): add an arrow in the middle of a link

--- src/app/Components/edit-project/workspace/drawable/DrawableLink.ts
+++ src/app/Components/edit-project/workspace/drawable/DrawableLink.ts
@@ -9,6 +9,7 @@ export class DrawableLink {
   to: DrawableMachine;
   outputLinked: OutputMat;
   element: any;
+  arrowElement: any;
   color: string;
 
   constructor(from: DrawableMachine, to: DrawableMachine, opt = {color: 'black', output: null}) {
@@ -19,6 +20,15 @@ export class DrawableLink {
   }
 
   draw(layer: any) {
+    const arrow = new Konva.Arrow({
+      stroke: this.color,
+      fill: this.color,
+      points: [this.from.posX(), this.from.posY(), (this.from.posX() + this.to.posX()) * 0.5, (this.from.posY() + this.to.posY()) * 0.5],
+      tension: 1,
+      pointerLength: 10,
+      pointerWidth: 12
+    });
+    layer.add(arrow);
     const line = new Konva.Arrow({
       stroke: this.color,
       fill: this.color,
@@ -29,12 +39,16 @@ export class DrawableLink {
     });
     layer.add(line);
     line.moveToBottom();
+    arrow.moveToBottom();
     this.element = line;
+    this.arrowElement = arrow;
     layer.draw();
   }
 
   move(layer) {
     this.element.points([this.from.posX(), this.from.posY(), this.to.posX(), this.to.posY()]);
+    this.arrowElement.points([this.from.posX(), this.from.posY(),
+      (this.from.posX() + this.to.posX()) * 0.5, (this.from.posY() + this.to.posY()) * 0.5]);
     layer.draw();
   }
 
@@ -42,6 +56,7 @@ export class DrawableLink {
     this.from.removeLink(this);
     this.to.removeLink(this);
     this.element.remove();
+    this.arrowElement.remove();
     layer.draw();
   }
 }
